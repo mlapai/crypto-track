dad91bb6a6182f90d5db49ca9b16fac2f74ef39c
Mladen Paic <paic.m@live.com>
2018-09-23 19:12:10 +0200

    store management

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.css'
 import VueToastr2 from 'vue-toastr-2'
 import 'vue-toastr-2/dist/vue-toastr-2.min.css'
+import Vuex from 'vuex'
+import VuexPersist from 'vuex-persist'
 
 Vue.config.productionTip = false
 
@@ -14,10 +16,36 @@ window.toastr = require('toastr')
 
 Vue.use(Vuetify)
 Vue.use(VueToastr2)
+Vue.use(Vuex)
+
+let state = {
+  userCryptos: []
+}
+
+const vuexLocalStorage = new VuexPersist({
+  key: 'crypto-track',
+  storage: window.localStorage
+})
+
+const store = new Vuex.Store({
+  state: state,
+  plugins: [vuexLocalStorage.plugin],
+  mutations: {
+    pushToUserCryptos (state, payload) {
+      // mutate state
+      state.userCryptos.push(payload)
+    },
+    changeCryptoValue (state, { currName, value }) {
+      let storageCurrency = state.userCryptos.find((curr) => { return curr.name === currName })
+      storageCurrency.value = value
+    }
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   components: { App },
   template: '<App/>'
