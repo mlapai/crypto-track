11dc44068792242a4461577887909f0aab879fe7
Mladen Paic <paic.m@live.com>
2018-09-23 12:52:21 +0200

    jquery, toastr, vuetify, axios dependencies installed

--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.css'
+import VueToastr2 from 'vue-toastr-2'
+import 'vue-toastr-2/dist/vue-toastr-2.min.css'
 
 Vue.config.productionTip = false
+
+window.toastr = require('toastr')
+
+Vue.use(Vuetify)
+Vue.use(VueToastr2)
 
 /* eslint-disable no-new */
 new Vue({
