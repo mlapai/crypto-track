ac8dfa90d6e8549ec4462b8f29390f226cdeb7eb
Mladen Paic <paic.m@live.com>
2018-09-23 12:53:47 +0200

    Home page data fetching

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,113 +1,105 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+  <v-data-table
+    :headers="headers"
+    :items="cryptos"
+    :total-items="totalItems"
+    :loading="loading"
+    v-bind:pagination.sync="pagination"
+    v-bind:rowsPerPageItems="rowsPerPageChoices"
+    class="elevation-1"
+  >
+      <template slot="items" slot-scope="props">
+          <td class="text-xs-left">{{ props.item.name }}</td>
+          <td class="text-xs-left">{{ props.item.symbol }}</td>
+          <td class="text-xs-left">{{ props.item.quotes.USD.price.toFixed(2) }}</td>
+          <td class="text-xs-left"
+              v-bind:style="[(props.item.quotes.USD.percent_change_24h < 0) ? {color: 'red'} : {color: 'green'}]">
+              {{ props.item.quotes.USD.percent_change_24h }} %
+          </td>
+          <td class="text-xs-left"></td>
+          <td class="text-xs-left"></td>
+      </template>
+  </v-data-table>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'Home',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  data: () => ({
+    headers: [
+      { text: 'Name', value: 'name' },
+      { text: 'Short name', value: 'symbol' },
+      { text: '$ Value', value: 'quotes.USD.price' },
+      { text: 'last 24h', value: 'quotes.USD.percent_change_24h' },
+      { text: 'Amount you own', value: '' },
+      { text: '$ value of your coin', value: '' }
+    ],
+    rowsPerPageChoices: [10, 25],
+    loading: false,
+    sort: 'rank',
+    start: 1,
+    totalItems: 50,
+    pagination: {},
+    cryptos: []
+  }),
+  watch: {
+    pagination: {
+      handler () {
+        // Calculating starting point on api depending on page and limit
+        this.start = (this.pagination.page > 1)
+          ? (this.pagination.page * this.pagination.rowsPerPage - this.pagination.rowsPerPage)
+          : 1
+        this.fetchDataFromApi()
+      },
+      deep: true
+    }
+  },
+  methods: {
+    // Fetch request data
+    fetchDataFromApi () {
+      this.resolveDataFromApi()
+        .then(data => {
+          this.cryptos = data.cryptos
+        })
+        .catch((e) => {
+          this.$toastr.error(e)
+        })
+    },
+    // Resolve api request
+    resolveDataFromApi () {
+      this.loading = true
+      return new Promise((resolve, reject) => {
+        this.requestDataFromApi()
+          .then((res) => {
+            let cryptos = Object.values(res.data.data)
+            // Timeout just for demonstration of loader
+            setTimeout(() => {
+              this.loading = false
+              resolve({
+                cryptos
+              })
+            }, 1000)
+          })
+          .catch(() => {
+            // Timeout just for demonstration of loader
+            setTimeout(() => {
+              this.loading = false
+              reject(new Error('Failed to fetch api data.'))
+            }, 1000)
+          })
+      })
+    },
+    // Requesting data from api
+    requestDataFromApi () {
+      return axios.get('https://api.coinmarketcap.com/v2/ticker/', {
+        params: {
+          start: this.start,
+          limit: this.pagination.rowsPerPage,
+          sort: this.sort
+        }
+      })
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
